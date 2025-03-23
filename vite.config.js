9de2c1dca144487bd233ba18c5e65846f041d822
kishoremk05 <kishore.05mk@gmail.com>
2025-03-23 21:22:21 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  base: '/portfolio/',  // Add this line for GitHub Pages deployment
+  plugins: [tailwindcss(), react()],
 })
