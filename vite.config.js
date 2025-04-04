2778f0e2225d7007430991b1a79bf7a08d09b681
kishoremk05 <kishore.05mk@gmail.com>
2025-04-04 22:55:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',  // Add this line for GitHub Pages deployment
+  base: './',  // Set base path to your GitHub repository name
   plugins: [tailwindcss(), react()],
 })
